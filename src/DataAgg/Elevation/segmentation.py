57b5f38c86124929cd44911506ced0c2626875af
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-10-12 15:04:09 -0600

    Move to SRTM DEM data in favor of AHN

--- src/DataAgg/Elevation/segmentation.py
+++ src/DataAgg/Elevation/segmentation.py
@@ -11,7 +11,7 @@ import aggregation as ag
 import numpy as np
 import cv2 as cv 
 import matplotlib.pyplot as plt
-
+from osgeo import gdal
 
 """
 1, 2, 3
@@ -31,17 +31,26 @@ DSM4 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37EN2\\r5_37en2.tif"
 DSM5 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37FN1\\r5_37fn1.tif"
 
 LAPTOPDSM = "C:\\Users\\siddh\\Documents\\DSMS\\R5_37FZ1\\r5_37fz1.tif"
-SRTM1 = "D:\\Documents\\School\\2019-20\\ISEF 2020\SRTM\\n51_e004_1arc_v3.tif"
-SRTM3 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n52_e004_1arc_v3.tif"
+
+SRTM1 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n52_e004_1arc_v3.tif"
+SRTM2 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n52_e005_1arc_v3.tif"
+SRTM3 = "D:\\Documents\\School\\2019-20\\ISEF 2020\SRTM\\n51_e004_1arc_v3.tif"
+SRTM4 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n51_e005_1arc_v3.tif"
+
 
 
 DENDSM1 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n39_w105_1arc_v3.tif"
 DENDSM2 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n39_w106_1arc_v3.tif"
 
 
-img1 = ag.load(DENDSM1, 0)
-img2 = ag.load(DENDSM2, 0)
-imgTot = np.hstack((img2, img1))
+img1 = ag.load(SRTM1, 0)
+img2 = ag.load(SRTM2, 0)
+img3 = ag.load(SRTM3, 0)
+img4 = ag.load(SRTM4, 0)
+
+hIm1 = np.hstack((img1, img2))
+hIm2 = np.hstack((img3, img4))
+imgTot = np.vstack((hIm1, hIm2))
 
 plt.imshow(imgTot)
 plt.show()
