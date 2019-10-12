8eab00bd7adad4381f1c2382a5e0c5b5bc790af2
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-10-12 15:19:51 -0600

    Foldername change

--- src/DataAggregation/Elevation/srtm.py
+++ src/DataAggregation/Elevation/srtm.py
@@ -13,25 +13,6 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 from osgeo import gdal
 
-"""
-1, 2, 3
-4, 5, 6
-7, 8, 9
-
-"""
-
-DSM9 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37FZ2\\r5_37fz2.tif"
-DSM6 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37FN2\\r5_37fn2.tif"
-DSM3 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_30HZ2\\r5_30hz2.tif"
-DSM2 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_30HZ1\\r5_30hz1.tif"
-DSM1 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_30GZ2\\r5_30gz2.tif"
-DSM8 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37FZ1\\r5_37fz1.tif"
-DSM7 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37EZ2\\r5_37ez2.tif"
-DSM4 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37EN2\\r5_37en2.tif"
-DSM5 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37FN1\\r5_37fn1.tif"
-
-LAPTOPDSM = "C:\\Users\\siddh\\Documents\\DSMS\\R5_37FZ1\\r5_37fz1.tif"
-
 SRTM1 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n52_e004_1arc_v3.tif"
 SRTM2 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n52_e005_1arc_v3.tif"
 SRTM3 = "D:\\Documents\\School\\2019-20\\ISEF 2020\SRTM\\n51_e004_1arc_v3.tif"
@@ -52,8 +33,12 @@ hIm1 = np.hstack((img1, img2))
 hIm2 = np.hstack((img3, img4))
 imgTot = np.vstack((hIm1, hIm2))
 
-plt.imshow(imgTot)
+
+den1 = ag.load(DENDSM1, 0)
+den2 = ag.load(DENDSM2, 0)
+dentotal = np.hstack((den2, den1))
+plt.imshow(dentotal)
 plt.show()
 
 #greyimg = cv.cvtColor(image, cv2.)
-print(ag.getBounds(DSM9))
+print(ag.getBounds(DENDSM2))
