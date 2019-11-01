6801be3a76f5dc95b7ee411e63913a6c904d10e6
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-11-01 17:35:36 -0600

    Add include files

--- src/TerrainGen/CPP/main.cpp
+++ src/TerrainGen/CPP/main.cpp
@@ -1,16 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <GL/glew.h>
-#include <GL/glfw.h>
-#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
 
-using namespace glm;
+int main(void)
+{
+    GLFWwindow* window;
 
-int main(void){
-   if (! gflwInit()){
-      fprintf(stderr, "Failed to initialize GLFW\n");
-      return -1;
-   }
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
-
-
