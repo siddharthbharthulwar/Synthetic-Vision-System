841e8ad2120418c0d74f0724fd950111d4f363fb
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-11-02 22:08:46 -0600

    Add Flight class for further continuous implementation

--- src/DataAggregation/AircraftPos/continuous.py
+++ src/DataAggregation/AircraftPos/continuous.py
@@ -1,3 +1,13 @@
 import math
 import pandas as pd 
 
+flightPath = "D:\\Documents\\School\\2019-20\\ISEF 2020\\FR24\\HV5022_228b0df3.csv"
+
+class Flight:
+    def __init__(self, pathVal):
+        self.path = pathVal
+    
+
+f1 = Flight(flightPath)
+print(f1.path)
+        
