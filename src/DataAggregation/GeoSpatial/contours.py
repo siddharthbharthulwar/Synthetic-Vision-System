680eef8ece8cbcc230852da8e4cbb68dc56e855f
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-11-06 20:14:36 -0700

    add terrain parsing cpp file

--- src/DataAggregation/GeoSpatial/contours.py
+++ src/DataAggregation/GeoSpatial/contours.py
@@ -44,4 +44,4 @@ ret,thresh = cv.threshold(rlimg,127,255,0)
 contours,hierarchy = cv.findContours(thresh, 1, 2)
 cnt = contours[0]
 M = cv.moments(cnt)
-print( M )
+print(M)
---
+++ src/TerrainGen/CPP/TerrainParse/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void InitializeTerrain(){
+    
+}
+
+
+int main(){
+    return 0;
+}
