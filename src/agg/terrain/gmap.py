1d56ff7851d5552ca5626a940850c8206e9dca8b
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-11-24 14:37:46 -0700

    Delete unnecessary files

--- src/agg/terrain/gmap.py
+++ src/agg/terrain/gmap.py
@@ -8,5 +8,16 @@ eham4 = "D:\\Documents\School\\2019-20\\ISEF 2020\\AHNEHAM\\R5_25CZ2\\r5_25cz2.t
 eham5 = "D:\\Documents\School\\2019-20\\ISEF 2020\\AHNEHAM\\R5_25DZ1\\r5_25dz1.tif"
 
 
-a = TerrainGrid((eham1, eham2, eham4, eham5), (2,2), 1)
-a.viewer_3d('gist_gray', -5, 30)
+SRTM1 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n52_e004_1arc_v3.tif"
+SRTM2 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n52_e005_1arc_v3.tif"
+SRTM3 = "D:\\Documents\\School\\2019-20\\ISEF 2020\SRTM\\n51_e004_1arc_v3.tif"
+SRTM4 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\SRTM\\n51_e005_1arc_v3.tif"
+
+
+
+
+l1 = r"D:\Documents\School\2019-20\ISEF 2020\SRTM\n39_w105_1arc_v3.tif"
+l2 = r"D:\Documents\School\2019-20\ISEF 2020\SRTM\n39_w106_1arc_v3.tif"
+a = TerrainGrid((l2, l1), (2, 1), 1)
+b = TerrainGrid((SRTM3), (1,1), 0)
+a.viewer_3d('gnuplot', 1500, 2500)
