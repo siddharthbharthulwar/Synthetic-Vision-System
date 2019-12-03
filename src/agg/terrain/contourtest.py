99dc0265b11afa4588bb0bef9cf41ccf88cfec02
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-12-02 20:32:34 -0700

    successful erosion and dilation to remove noise

--- src/agg/terrain/contourtest.py
+++ src/agg/terrain/contourtest.py
@@ -9,11 +9,11 @@ b = r"D:\Documents\School\2019-20\ISEF 2020\ML\PNG\r5_30hz2.png"
 
 img = cv.imread(b)
 imgray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-ret, thresh = cv.threshold(imgray, 50, 255, 0)
+ret, thresh = cv.threshold(imgray, 50, 255, cv.THRESH_BINARY_INV)
 
 im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+cv.drawContours(imgray, contours, -1, (0, 0, 255), 3)
 
 epsilon = 0.1 * cv.arcLength(contours[5], True)
 approx = cv.approxPolyDP(contours[5], epsilon, True)
@@ -21,6 +21,6 @@ print(len(contours))
 print(contours)
 print("break")
 print(approx)
-cv.imshow("Image", img)
+cv.imshow("Image", imgray)
 cv.waitKey(0)
 cv.destroyAllWindows()
