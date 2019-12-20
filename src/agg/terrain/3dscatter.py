4d18c2396c7c4e22f247366c1e29cd54b901abaa
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-12-20 10:26:39 -0700

    size based filtering

--- src/agg/terrain/3dscatter.py
+++ src/agg/terrain/3dscatter.py
@@ -1,6 +1,14 @@
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm 
+import matplotlib.pyplot as plt
 from matplotlib import colors 
+from aggregation import TerrainGrid
 
-fig = plt.figure()
-axis = fig.add_subplot(1, 1, )
+rasdf = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37hn2.tif"
+rd1 = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37fz1.tif"
+
+a = TerrainGrid((rd1), (1,1), 1)
+
+
+a.arrayValues =  15 * a.arrayValues[10500:12500, 3000:5000]
+a.viewer_3d('viridis', -5, 50)
--- src/agg/terrain/labelling.py
+++ src/agg/terrain/labelling.py
@@ -6,14 +6,14 @@ import numpy.ma as ma
 
 rd0 = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37ez2.tif"
 rd1 = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37fz1.tif"
+rasdf = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37hn2.tif"
 
 
+a = TerrainGrid((rd1), (1,1), 1)
 
-a = TerrainGrid((rd0, rd1), (2,1), 1)
-'''
 c = cv.threshold(a.arrayValues, 5, 200, cv.THRESH_BINARY)[1].astype('uint8')
 
-b = a.erodilate(5, np.ones((2,2), np.uint8) , 2).astype('uint8')
+b = a.erodilate(3.6, np.ones((2,2), np.uint8) , 1).astype('uint8')
 
 n_labels, labels, stats, centroids = cv.connectedComponentsWithStats(b, connectivity=4)
 
@@ -21,12 +21,19 @@ colors = np.random.randint(0, 255, size = n_labels, dtype = np.uint8)
 colors[0] = 0
 false_colors = colors[labels]
 
-print(np.amax(labels), np.amin(labels))
-print(labels)
+count = 0
+min_area = 100
+indexList = []
+while (count < n_labels):
+    if (stats[count, 4] > min_area):
+        indexList.append(count)
+        count = count + 1
+    else:
+        count = count + 1
 
-plt.imshow(labels)
+print(len(indexList))
+#the issue here is that the numpy labelling process isn't semantic. 
+plt.imshow(a.arrayValues, vmin = -10, vmax = 50)
+plt.imshow(ma.masked_values(labels, 0), cmap = 'gist_gray', alpha = 0.8)
 plt.show()
 
-'''
-plt.imshow(a.label(4.5, np.ones((2,2), dtype = 'uint8'), 1, 4))
-plt.show()
