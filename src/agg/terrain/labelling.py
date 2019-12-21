4ce3f2794e2949c012e6ecf5cb0e890754bb5519
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-12-21 08:39:38 -0700

    tests ran

--- src/agg/terrain/labelling.py
+++ src/agg/terrain/labelling.py
@@ -18,8 +18,7 @@ DSM4 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37EN2\\r5_37en2.tif"
 DSM5 = "D:\\Documents\\School\\2019-20\\ISEF 2020\\AHN\\R5_37FN1\\r5_37fn1.tif"
 
 
-a = TerrainGrid((rd0), (1,1), 1)
-a.arrayValues = a.arrayValues[7000:10000, 0:3500]
+a = TerrainGrid((rd1), (1,1), 1)
 a.show(-5, 50)
 c = cv.threshold(a.arrayValues, 5, 200, cv.THRESH_BINARY)[1].astype('uint8')
 
@@ -30,7 +29,7 @@ print(n_labels)
 
 retl = []
 start = time()
-minsize = 100
+minsize = 500
 for i in np.unique(labels):
     if (stats[i, 4] > minsize):
 
@@ -44,25 +43,20 @@ print(end - start ," seconds to complete task. ")
 print(retl)
 print(retl[1])
 
-n, bins, patches = plt.hist(retl, 250, facecolor='blue', alpha=0.5)
-plt.show()
 
 buildings = []
 vegetation = []
 
-count = 0
-while (count < retl.length):
-    if (retl[count] > 1.5):
-        vegetation.append(retl[count])
-        count = count + 1
-    else:
-        buildings.append(retl[count])
-        count + 1
-
-print(len(buildings), " buildings")
-print(len(vegetation), " vegetation")
 
 print("break")
+
+np.save("final", retl)
+
+
+
+n, bins, patches = plt.hist(retl, 250, facecolor='blue', alpha=0.5)
+plt.show()
+
 
 '''
 plt.imshow(np.zeros(labels.shape), cmap = 'gist_gray')
