20a6cd6d7432752c9d2b27a46ae2a425c4aedd70
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-12-24 09:51:03 -0700

    time elapsed

--- src/agg/terrain/labelling.py
+++ src/agg/terrain/labelling.py
@@ -4,7 +4,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy.ma as ma
 import random as rng
-from time import time
+import time as time
 
 rd0 = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37ez2.tif"
 rd1 = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37fz1.tif"
@@ -12,8 +12,8 @@ rasdf = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37hn2.tif"
 
 r2 = r"D:\Documents\School\2019-20\ISEF 2020\HighProcessed\r_37fz2.tif"
 
-a = TerrainGrid((rd0), (1,1), 1)
-a.arrayValues = a.arrayValues[8000:10000, 3000:5000]
+a = TerrainGrid((rd1), (1,1), 1)
+a.arrayValues = a.arrayValues[6000:10000, 0:4000]
 
 c = cv.threshold(a.arrayValues, 2.9, 200, cv.THRESH_BINARY)[1].astype('uint8')
 
@@ -25,12 +25,6 @@ n_labels, labels, stats, centroids = cv.connectedComponentsWithStats(b, connecti
 print(n_labels)
 
 
-
-plt.imshow(a.arrayValues)
-
-
-plt.imshow(ma.masked_values(labels, 0), cmap = 'gist_gray', vmin = 0, vmax = 1)
-plt.show()
 
 
 variance = []
@@ -47,13 +41,14 @@ unique = np.delete(np.unique(labels), 0)
 incount = 0
 
 newArray = np.zeros(labels.shape, np.uint8)
+start = time.time()
 for i in unique:
     if (stats[i, 4] > 500):
         print(i)
         org = ma.masked_not_equal(labels, i) / i
         var = np.var((org) * a.arrayValues)
         variance.append(var)
-        if (var > 6):
+        if (var > 2.76):
             vegetation.append(i)
             invegetation.append(incount)
             incount +=1 
@@ -65,6 +60,8 @@ for i in unique:
             newArray = np.add(newArray, 5 * org.filled(0))
 
 print(len(buildings), len(vegetation), len(variance))
+end = time.time()
+print(end - start ," seconds elapsed cuh.")
 
 '''
 count = 0
@@ -80,7 +77,7 @@ while (count < len(vegetation)):
     print("Real index of: ", vegetation[count], " and relative index of: ", invegetation[count], " with variance of: ", variance[invegetation[count]], " (V)")
     plt.show()
     count +=1
-    '''
+'''
 plt.imshow(a.arrayValues)
 plt.imshow(ma.masked_values(newArray, 0), cmap = 'tab10', vmin = 0, vmax = 19)
 plt.show()
