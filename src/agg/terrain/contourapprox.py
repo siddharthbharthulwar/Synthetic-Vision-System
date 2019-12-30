4f6190ce205f54e331466101b97db168dad2adbb
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2019-12-29 23:54:35 -0700

    rasterio shape features doesn't seem too promising

--- src/agg/terrain/contourapprox.py
+++ src/agg/terrain/contourapprox.py
@@ -4,6 +4,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy.ma as ma
 import random as rng
+import shapely
 
 path = r"C:\Users\siddh\Desktop\raster.png"
 
@@ -14,7 +15,9 @@ plt.show()
 ret, thresh = cv.threshold(img, 200, 1, cv.THRESH_BINARY)
 im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-final = cv.drawContours(thresh, contours, contourIdx= 1, color = 255, thickness= 2)
+final = cv.drawContours(thresh, contours, contourIdx= 1, color = 255, thickness= 1)
 
 plt.imshow(final)
 plt.show()
+
+
