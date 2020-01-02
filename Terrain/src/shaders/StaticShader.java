56fa819cbee36882ab53b0524b50dba25d8fbe18
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-01-01 18:32:03 -0700

    Add useful graphical methods

--- Terrain/src/shaders/StaticShader.java
+++ Terrain/src/shaders/StaticShader.java
@@ -1,9 +1,13 @@
 package shaders;
- 
+
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram{
      
     private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+    
+    private int location_transformationMatrix;
  
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -14,7 +18,16 @@ public class StaticShader extends ShaderProgram{
         super.bindAttribute(0, "position");
         super.bindAttribute(1, "textureCoords");
     }
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+		
+	}
      
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_transformationMatrix, matrix);
+	}
      
  
 }
--- Terrain/src/shaders/vertexShader.txt
+++ Terrain/src/shaders/vertexShader.txt
@@ -5,6 +5,8 @@ in vec2 textureCoords;
 
 out vec2 pass_textureCoords;
 
+uniform mat4 transformationMatrix;
+
 void main(void){
 
 	gl_Position = vec4(position,1.0);
