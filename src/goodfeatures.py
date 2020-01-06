a8060b979d99d8ac49073cf364e8f3c929737912
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-01-05 23:50:15 -0700

    cv.goodfeatures also looks promising but too many corners detected

---
+++ src/goodfeatures.py
@@ -0,0 +1,23 @@
+from terraingrid import TerrainGrid
+import numpy as np 
+import cv2 as cv 
+import matplotlib.pyplot as plt
+import numpy.ma as ma
+import random as rng
+import shapely
+
+path = r"C:\Users\siddh\Projects\Synthetic Vision System\raster.png"
+
+img = cv.imread(path, cv.IMREAD_GRAYSCALE)
+
+plt.imshow(img)
+plt.show()
+
+
+corners = cv.goodFeaturesToTrack(img, 25, 0.01, 10)
+
+for i in corners:
+    x,y = i.ravel()
+    cv.circle(img,(x,y),3,123213,-1)
+plt.imshow(img)
+plt.show()
