a09bd5eaa0a9cc4b4c1d82460c293b489f3fc1c6
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-01-05 23:46:03 -0700

    Harris Corner Detection (looks promising)

--- src/harriscorner.py
+++ src/harriscorner.py
@@ -13,17 +13,9 @@ img = cv.imread(path, cv.IMREAD_GRAYSCALE)
 plt.imshow(img)
 plt.show()
 ret, thresh = cv.threshold(img, 200, 1, cv.THRESH_BINARY)
-im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-cnt = contours[0]
 
-perimeter = cv.arcLength(cnt, True)
-epsilon = 0.01 * perimeter
-approx = cv.approxPolyDP(cnt, epsilon, True)
+dst = cv.cornerHarris(thresh, 4, 3, 0.04)
 
-hull = cv.convexHull(cnt)
-
-final = cv.drawContours(thresh,contours, contourIdx= -1, color = 255, thickness= 1)
-
-plt.imshow(final)
+plt.imshow(dst)
 plt.show()
