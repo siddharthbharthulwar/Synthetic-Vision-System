3ab3025a189ea0be0a6fc0d87bc16c7bfea733bf
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-02-05 23:32:17 -0700

    initialize navigation utils package

---
+++ Terrain/src/navigation/cameraComponents.java
@@ -0,0 +1,19 @@
+package navigation;
+
+public class cameraComponents {
+
+	private float xComponent;
+	private float yComponent;
+	private float zComponent;
+	private float rotation;
+	
+	public cameraComponents(float xComponent, float yComponent, float zComponent, float rotation) {
+		
+		this.xComponent = xComponent;
+		this.yComponent = yComponent;
+		this.zComponent = zComponent;
+		this.rotation = rotation;
+	}
+	
+	
+}
---
+++ Terrain/src/navigation/linearDecomposition.java
@@ -0,0 +1,11 @@
+package navigation;
+
+import org.lwjgl.util.vector.Vector3f;
+
+public class linearDecomposition {
+	
+	public static cameraComponents linearDecomp(Vector3f pos1, Vector3f pos2) {
+		return new cameraComponents(0, 0, 0, 0);
+	}
+
+}
