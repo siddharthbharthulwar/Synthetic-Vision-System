40c16cf2579b524c12060378636b3bf616aa7d20
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-03-01 16:04:15 -0700

    begin line parsing

--- Terrain/src/data/SimData.java
+++ Terrain/src/data/SimData.java
@@ -8,7 +8,49 @@ import java.util.Scanner;
 
 
 public class SimData {
+	
+	private List<Float> latitudes;
+	private List<Float> longitudes;
+	private List<Float> times;
+	private List<Float> velocityTrue;
+	private List<Float> pitchDeg;
+	private List<Float> rollDeg;
+	private List<Float> alphaDeg;
+	private List<Float> hpathDeg;
+	private List<Float> vpathDeg;
+	private List<Float> altitude;
+	private List<Float> vX;
+	private List<Float> vY;
+	private List<Float> vZ;
+	private List<Float> distNM;
+	
+	public SimData(String path) {
+		
+		File file = new File(path);
+		Scanner sc = null;
+		List<String> lines = new ArrayList<String>();
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()){
+				String str = sc.nextLine();
+			    lines.add(str);
+			}
+			   
+		} catch (IOException  exp) {
+			exp.printStackTrace();
+		}
+			  sc.close();
+			  System.out.println(lines.get(0));
 
+	}
+	
+	public List<Float> ParseLine(String line){
+		
+		return null;
+	}
+	
+	/*
 	public static void main(String[] args) {
 		
 	File file = new File("res/sensors.txt");
@@ -32,4 +74,5 @@ public class SimData {
 		  System.out.println(lines.get(0));
 
 	}
+	*/
 }
